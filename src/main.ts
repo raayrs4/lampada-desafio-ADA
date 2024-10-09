7c15f03381557eff151b65035aed0b85dc1185ed
Rayane <raay.rs4@gmail.com>
2024-10-09 19:15:24 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+const lampada = document.querySelector<HTMLImageElement>("#lampada")
+
+let estadoLampada: "apagada" | "acesa" | "quebrada" = "apagada";
+let contador: number = 0;
+let clickTimeout: number | undefined;
+
+function toggleLamp(): void {
+    if (estadoLampada === "quebrada") {
+      return;
+    }
+    
+    contador++;
+    
+    // Limpa o temporizador anterior e começa um novo
+    if (clickTimeout !== undefined) {
+      clearTimeout(clickTimeout);
+    }
+    if (contador > 5) {
+      estadoLampada = "quebrada";
+      atualizarImagem(); 
+      setTimeout(() => {
+        alert("Dedinho nervoso, a lâmpada quebrou e não pode mais ser acesa!");
+    }, 200); 
+      return;
+    }
+    
+    if (estadoLampada === "apagada") {
+      estadoLampada = "acesa";
+    } else if (estadoLampada === "acesa") {
+      estadoLampada = "apagada"; 
+    }
+    
+    atualizarImagem();
+    
+    clickTimeout = setTimeout(() => {
+        contador = 0; // Reseta o contador de cliques
+    }, 5000);
+}
+
+function atualizarImagem() {
+  if(lampada) {
+    if (estadoLampada === "apagada") {
+        lampada.src = "./public/images/apagada.png";
+    } else if (estadoLampada === "acesa") {
+        lampada.src = "./public/images/acesa.png";
+    } else if (estadoLampada === "quebrada") {
+      lampada.src = "./public/images/quebrada.png";
+    }
+  }
+}
+
+(window as any).toggleLamp = toggleLamp;
